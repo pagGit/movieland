5e9929b16795a0fef571d2652e21e18be3b09851
Andrii P <pag_mail@ukr.net>
2018-11-14 09:52:39 +0200

    [b-1] Get all movies without test cases

---
+++ src/main/java/com/pager/movieland/entity/Movie.java
@@ -0,0 +1,80 @@
+package com.pager.movieland.entity;
+
+public class Movie {
+    private int id;
+    private String nameRussian;
+    private String nameNative;
+    private int yearOfRelease;
+    private double rating;
+    private double price;
+    private String picturePath;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNameRussian() {
+        return nameRussian;
+    }
+
+    public void setNameRussian(String nameRussian) {
+        this.nameRussian = nameRussian;
+    }
+
+    public String getNameNative() {
+        return nameNative;
+    }
+
+    public void setNameNative(String nameNative) {
+        this.nameNative = nameNative;
+    }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getPicturePath() {
+        return picturePath;
+    }
+
+    public void setPicturePath(String picturePath) {
+        this.picturePath = picturePath;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", nameRussian='" + nameRussian + '\'' +
+                ", nameNative='" + nameNative + '\'' +
+                ", yearOfRelease=" + yearOfRelease +
+                ", rating=" + rating +
+                ", price=" + price +
+                ", picturePath='" + picturePath + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/pager/movieland/service/MovieService.java
@@ -0,0 +1,9 @@
+package com.pager.movieland.service;
+
+import com.pager.movieland.entity.Movie;
+
+import java.util.List;
+
+public interface MovieService {
+    List<Movie> getAll();
+}
