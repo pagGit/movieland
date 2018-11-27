52d5352a89099e90319c27bf0475b1ca33c10737
Andrii P <pag_mail@ukr.net>
2018-11-27 19:20:11 +0200

    lb1-liquibase

--- src/main/java/com/pager/movieland/dao/jdbc/JdbcMovieDao.java
+++ src/main/java/com/pager/movieland/dao/jdbc/JdbcMovieDao.java
@@ -12,7 +12,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Map;
 
 @Repository
 public class JdbcMovieDao implements MovieDao {
