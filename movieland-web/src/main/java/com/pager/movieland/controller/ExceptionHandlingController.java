94b0d207b43654e865f57fc9dd448cef581eafc9
Andrii P <pag_mail@ukr.net>
2018-12-11 10:08:46 +0200

    [s-1] Security token service changed @Controller annotation to @ControllerAdvice for ExceptionHandlingController

--- movieland-web/src/main/java/com/pager/movieland/controller/ExceptionHandlingController.java
+++ movieland-web/src/main/java/com/pager/movieland/controller/ExceptionHandlingController.java
@@ -4,11 +4,11 @@ import com.pager.movieland.service.exception.AuthenticationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@Controller
+@ControllerAdvice
 public class ExceptionHandlingController {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
